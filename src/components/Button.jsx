92a3ca506ed284d4cd3cf5c212bd270adfb46a6e
vedant-rmgd <vedant.ramgade@gmail.com>
2025-05-24 13:59:13 +0530

    create reusable UI components: input & button

---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Button({
+  children, //text
+  type = "button",
+  bgColor = "bg-blue-600",
+  textcolor = "text-white",
+  className = "",
+  ...props
+}) {
+  return (
+    <button
+      type={type}
+      className={`px-4 py-2 rounded-lg ${bgColor} ${textcolor} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
