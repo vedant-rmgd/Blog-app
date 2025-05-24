54b1c2fba8c63ac92f6241a153258703033973d0
vedant-rmgd <vedant.ramgade@gmail.com>
2025-05-24 23:51:17 +0530

    add login form

--- src/components/index.js
+++ src/components/index.js
@@ -7,5 +7,6 @@ import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
 import PostCard from "./PostCard";
+import Login from "./Login";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select, PostCard };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select, PostCard, Login };
