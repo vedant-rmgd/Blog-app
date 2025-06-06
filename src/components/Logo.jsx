5ecb1b432049916c6ef0538b7fb9d9570a35c59a
vedant-rmgd <vedant.ramgade@gmail.com>
2025-06-06 21:24:30 +0530

    fixed bugs related to deployment

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-
 function Logo({width = '100px'}) {
-
-  const imageUrl = "src/components/png-transparent-blog-blooger-communication-internet-media-social-technology-social-media-logos-ii-flat-colorful-icon-thumbnail-removebg-preview.png"
 
   return (
     <div>
-      <img src={`${imageUrl ? imageUrl : null}`}  className='rounded-xl w-12'/>
+      <img src="/App-logo.png"  className='rounded-xl w-12'/>
     </div>
   )
 }
